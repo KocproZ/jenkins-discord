a951b5c37e49b88b5150faa754423be6736fdda7
James Upjohn <jspartan250@gmail.com>
2017-09-18 19:32:02 +1200

    Version bump

--- src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
+++ src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
@@ -28,7 +28,7 @@ public class WebhookPublisher extends Notifier {
     private final boolean enableUrlLinking;
     private final boolean enableArtifactList;
     private static final String NAME = "Discord Notifier";
-    private static final String VERSION = "1.0.0";
+    private static final String VERSION = "1.1.0";
 
     @DataBoundConstructor
     public WebhookPublisher(String webhookURL, boolean sendOnStateChange, boolean enableUrlLinking, boolean enableArtifactList) {
