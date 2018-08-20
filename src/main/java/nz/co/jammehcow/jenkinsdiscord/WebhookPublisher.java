bb94b8f60091145c19658cb168a67cbb194ef465
James Upjohn <jspartan250@gmail.com>
2018-08-20 16:11:31 +1200

    Update versions in files

--- src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
+++ src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
@@ -36,7 +36,7 @@ public class WebhookPublisher extends Notifier {
     private final boolean enableArtifactList;
     private final boolean enableFooterInfo;
     private static final String NAME = "Discord Notifier";
-    private static final String VERSION = "1.1.1";
+    private static final String VERSION = "1.3.0";
 
     @DataBoundConstructor
     public WebhookPublisher(String webhookURL, String thumbnailURL, boolean sendOnStateChange, String statusTitle, String branchName, boolean enableUrlLinking, boolean enableArtifactList, boolean enableFooterInfo) {
