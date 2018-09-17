3ff06d3cd31d5df3595796307557d80882836675
Ave <ave@ave.zone>
2018-09-17 11:12:13 +0300

    I hate java

--- src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
+++ src/main/java/nz/co/jammehcow/jenkinsdiscord/WebhookPublisher.java
@@ -146,7 +146,7 @@ public class WebhookPublisher extends Notifier {
         public boolean isApplicable(Class<? extends AbstractProject> aClass) { return true; }
 
         public FormValidation doCheckWebhookURL(@QueryParameter String value) {
-            if (!value.matches("https://(canary\.|ptb\.|)discordapp\\.com/api/webhooks/\\d{18}/(\\w|-|_)*(/?)"))
+            if (!value.matches("https://(canary\\.|ptb\\.|)discordapp\\.com/api/webhooks/\\d{18}/(\\w|-|_)*(/?)"))
                 return FormValidation.error("Please enter a valid Discord webhook URL.");
             return FormValidation.ok();
         }
